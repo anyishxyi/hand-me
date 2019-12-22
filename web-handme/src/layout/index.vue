600a29ddc2bcb378c2c75a2773e3ddd028db1422
casison7 <jeanpaul@ownest.io>
2019-12-22 20:35:57 +0100

    fix rm sidebar

--- web-handme/src/layout/index.vue
+++ web-handme/src/layout/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <div :class="{hasTagsView:needTagsView}" class="main-container">
+    <div :class="{hasTagsView:needTagsView}">
       <div :class="{'fixed-header':fixedHeader}">
         <navbar />
         <tags-view v-if="needTagsView" />
@@ -51,14 +51,9 @@ export default {
 
   .app-wrapper {
     @include clearfix;
-    position: relative;
+    position: fixed;
     height: 100%;
     width: 100%;
-
-    &.mobile.openSidebar {
-      position: fixed;
-      top: 0;
-    }
   }
 
   .drawer-bg {
@@ -71,18 +66,18 @@ export default {
     z-index: 999;
   }
 
-  .fixed-header {
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 9;
-    width: calc(100% - #{$sideBarWidth});
-    transition: width 0.28s;
-  }
+  // .fixed-header {
+  //   position: fixed;
+  //   top: 0;
+  //   right: 0;
+  //   z-index: 9;
+  //   width: calc(100% - #{$sideBarWidth});
+  //   transition: width 0.28s;
+  // }
 
-  .hideSidebar .fixed-header {
-    width: calc(100% - 54px)
-  }
+  // .hideSidebar .fixed-header {
+  //   width: calc(100% - 54px)
+  // }
 
   .mobile .fixed-header {
     width: 100%;
