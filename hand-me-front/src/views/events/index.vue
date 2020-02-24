92db42b80980be71f987be2949299c264e9fb106
n19l <jeanpaul@ownest.io>
2020-02-24 17:45:59 +0100

    WIP events page

---
+++ hand-me-front/src/views/events/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="events-page">
+    hello im events page
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      console.log('events page')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.events-page {
+
+}
+</style>
