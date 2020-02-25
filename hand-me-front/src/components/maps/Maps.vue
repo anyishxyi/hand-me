a9361a2b2a09aea535c518e9e8f0ffd5fb50745b
n19l <jeanpaul@ownest.io>
2020-02-25 03:40:44 +0100

    WIP add maps on events page

---
+++ hand-me-front/src/components/maps/Maps.vue
@@ -0,0 +1,73 @@
+<template>
+  <GoogleMapLoader
+    :mapConfig="mapConfig"
+    apiKey="apiKey"
+  >
+    <template slot-scope="{ google, map }">
+      <GoogleMapMarker
+        v-for="marker in markers"
+        :key="marker.id"
+        :marker="marker"
+        :google="google"
+        :map="map"
+      />
+    </template>
+  </GoogleMapLoader>
+</template>
+
+<script>
+import GoogleMapLoader from "./GoogleMapLoader";
+import GoogleMapMarker from "./GoogleMapMarker";
+
+import { mapSettings } from "@/constants/mapSettings";
+
+export default {
+  components: {
+    GoogleMapLoader,
+    GoogleMapMarker
+  },
+
+  data() {
+    return {
+      apiKey: 'AIzaSyAe1WPBLhSasHA1CvdEpkbOqqGOTB4d3pM',
+      markers: [
+        {
+          id: "a",
+          position: { lat: 3, lng: 101 }
+        },
+        {
+          id: "b",
+          position: { lat: 5, lng: 99 }
+        },
+        {
+          id: "c",
+          position: { lat: 6, lng: 97 }
+        }
+      ],
+      lines: [
+        {
+          id: "1",
+          path: [{ lat: 3, lng: 101 }, { lat: 5, lng: 99 }]
+        },
+        {
+          id: "2",
+          path: [{ lat: 5, lng: 99 }, { lat: 6, lng: 97 }]
+        }
+      ]
+    };
+  },
+
+  computed: {
+    mapConfig() {
+      return {
+        ...mapSettings,
+        center: this.mapCenter
+      };
+    },
+
+    mapCenter() {
+      return this.markers[1].position;
+    }
+  }
+};
+</script>
--- hand-me-front/src/views/events/index.vue
+++ hand-me-front/src/views/events/index.vue
@@ -1,11 +1,56 @@
 <template>
-  <div class="events-page">
-    hello im events page
+  <div class="events-container">
+    <div class="navbar navbar-default shadow">
+      <div class="right-menu">
+        <el-button type="text">
+          <router-link :to="{path: '/home'}">
+            Accueil
+          </router-link>
+        </el-button>
+        <el-button type="text">
+          <router-link :to="{path: '/events'}">
+            Evenements
+          </router-link>
+        </el-button>
+        <el-button type="text">
+          <router-link :to="{path: '/register'}">
+            Inscription
+          </router-link>
+        </el-button>
+        <el-button type="primary">
+          <router-link :to="{path: '/login'}">
+            Connexion
+          </router-link>
+        </el-button>
+      </div>
+    </div>
+    <div class="container">
+      <div class="box">
+        <div class="box-row">
+          <div class="box-cell left">
+            <div class="events_filter">
+
+            </div>
+            <div class="events_list">
+
+            </div>
+          </div>
+          <div class="box-cell right">
+            <Maps />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Maps from '@/components/maps/Maps'
+
 export default {
+  components: {
+    Maps
+  },
   data(){
     return {
 
@@ -23,7 +68,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.events-page {
 
+.events-container {
+  width:100%;
+}
+
+.container .box {
+  width:100%;
+  height:100%;
+  margin-top:60px;
+  display:table;
+}
+.container .box .box-row {
+  display:table-row;
+}
+.container .box .box-cell {
+  display:table-cell;
+  padding:10px;
+}
+.container .box .box-cell.left {
+  color:white;
+  text-align:justify;
+  width:40%;
+}
+.container .box .box-cell.right {
+  text-align:justify
 }
 </style>
