35a9b91fb6c40fb4e9cd59b6e25530bea04f009c
n19l <jeanpaulngalula07@gmail.com>
2020-05-10 01:46:15 +0200

    clear events list

--- hand-me-front/src/views/events/index.vue
+++ hand-me-front/src/views/events/index.vue
@@ -38,7 +38,7 @@
             >
             </Places>
             <el-date-picker
-              class="category"
+              class="dates"
               v-model="dates"
               type="daterange"
               range-separator="A"
@@ -48,26 +48,7 @@
           </div>
         </div>
         <div class="events_list clear">
-          <ol class="SD4Ugf">
-            <li class="lPyEac">
-              <div class="MebuN">
-                <div class="L32YH"></div>
-                <div class="tsAU4e">
-                  <div class="wIuJz">
-                    <h3 class="W6bZuc YMlIz">Londres</h3>
-                    <div class="ZjDced CQYfx">
-                      <span class="nx0jzf">Sans escale</span>
-                      <span class="qeoz6e U325Rc">::before</span>
-                      <span class="Xq1DAb">1h10</span>
-                    </div>
-                  </div>
-                  <div class="Q70fcd sSHqwe">
-                    66 e
-                  </div>
-                </div>
-              </div>
-            </li>
-          </ol>
+
         </div>
       </div>
       <div class="right">
@@ -224,14 +205,10 @@ export default {
           float:left;
           width:50%;
         }
-        .category {
+        .dates {
           float: right;
           width:50%;
         }
-      }
-
-      .dates {
-        width: 100%;
       }
     }
 
@@ -256,102 +233,4 @@ export default {
   clear:both;
 }
 
-////////
-
-.SD4Ugf {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-.MebuN {
-  display: flex;
-  height: 118px;
-  border: 0;
-  border-radius: 8px;
-  box-shadow: 0 1px 2px 0 rgba(60,64,67,.30), 0 1px 3px 1px rgba(60,64,67,.15);
-  margin: 2px 2px 8px;
-  position: relative;
-  overflow: hidden;
-  cursor: pointer;
-}
-
-.L32YH {
-  background: no-repeat center/cover;
-  width: 118px;
-  flex-shrink: 0;
-}
-
-.tsAU4e {
-  display: flex;
-  flex: 4 1 auto;
-  flex-direction: column;
-  justify-content: space-between;
-  line-height: 20px;
-  min-width: 0;
-  padding: 16px 16px 11px;
-}
-
-.Q70fcd {
-  align-items: flex-end;
-  align-self: flex-end;
-  display: flex;
-  flex-direction: column;
-  max-width: 100%;
-}
-
-.sSHqwe {
-  font: 400 12px/16px Roboto,Arial,sans-serif;
-  letter-spacing: .3px;
-  color: #5f6368;
-}
-
-.W6bZuc {
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-
-.YMlIz {
-  font: 500 16px/24px 'Google Sans',Roboto,Arial,sans-serif;
-  letter-spacing: .1px;
-  color: #3c4043;
-}
-
-.ZjDced {
-  align-items: center;
-  display: flex;
-}
-
-.CQYfx {
-  font: 400 14px/20px Roboto,Arial,sans-serif;
-  letter-spacing: .2px;
-  color: #5f6368;
-}
-
-.nx0jzf {
-  flex: 0 1 auto;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-
-.U325Rc {
-  margin: 0 4px;
-}
-
-.Xq1DAb {
-  white-space: nowrap;
-}
-
-li {
-  display: list-item;
-  text-align: -webkit-match-parent;
-}
-
-.qeoz6e::before {
-  content: "\0000b7";
-}
 </style>
