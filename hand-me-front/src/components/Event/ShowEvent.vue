50888a1c09d51c1e8325e2c55bb26310b45dafa9
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-10-23 07:02:00 +0200

    wip

--- hand-me-front/src/components/Event/ShowEvent.vue
+++ hand-me-front/src/components/Event/ShowEvent.vue
@@ -12,21 +12,13 @@
           <span class="infos_name">Emma Watson</span>
           <span class="infos_nick">@EmWatson</span>
 
-          <a href="#" @click="participate"></a>
+          <a v-if="loggedIn" href="#" @click="participate"></a>
         </div>
 
         <div class="card-profile_user-stats">
           <div class="stats-holder">
             <div class="user-stats">
-              <strong>Tweets</strong>
-              <span>1,337</span>
-            </div>
-            <div class="user-stats">
-              <strong>Following</strong>
-              <span>561</span>
-            </div>
-            <div class="user-stats">
-              <strong>Followers</strong>
+              <strong>Participants</strong>
               <span>718</span>
             </div>
           </div>
