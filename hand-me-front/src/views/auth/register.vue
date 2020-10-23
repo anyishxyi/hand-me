12c9e0159302cf4dfab7921e043cea93fb3cbdf6
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-10-23 02:15:42 +0200

    wip

--- hand-me-front/src/views/auth/register.vue
+++ hand-me-front/src/views/auth/register.vue
@@ -50,10 +50,17 @@
           </div>
         </div>
         <div v-if="step13">
-          <p>Votre compte a été créé avec succès !</p>
-          <router-link :to="{path: '/events'}"><el-button>continuer</el-button></router-link>
+          <div class="success-checkmark">
+            <div class="check-icon">
+              <span class="icon-line line-tip"></span>
+              <span class="icon-line line-long"></span>
+              <div class="icon-circle"></div>
+              <div class="icon-fix"></div>
+            </div>
+          </div>
+          <div class="message">Votre compte a été créé avec succès !</div>
+          <router-link :to="{path: '/events'}"><el-button class="success">continuer</el-button></router-link>
         </div>
-        <!-- Steps for Association registion -->
         <div v-if="step1">
           <p><strong>Quelques informations sur votre association</strong></p>
           <p><el-input placeholder="SIRET..." v-model="orgaData.organizationMatricule" class="marginInput"></el-input></p>
@@ -286,7 +293,7 @@ export default {
     async handleValidateOrganization() {
       this.orgaData.organizationEmail = this.registerForm.email
       this.orgaData.organizationPassword = this.registerForm.pass
-      this.$store.dispatch('register_association', this.userData)
+      this.$store.dispatch('register_association', this.orgaData)
                   .then(({ data }) => { console.log('orga data is:', data ) })
                   .catch(error => { console.log(error) })
       // console.log('res', res)
@@ -426,6 +433,173 @@ export default {
 
 .clear {
   clear:both;
+}
+
+.success {
+  margin-left: 56%;
+  margin-top: 5%;
+}
+
+.message {
+  padding-top: 15%;
+  padding-left: 35%;
+  font-size: 24px;
+}
+
+.success-checkmark {
+    width: 80px;
+    height: 115px;
+    margin: 0 auto;
+    padding-left: 15%;
+    padding-top: 20%;
+
+    .check-icon {
+        width: 80px;
+        height: 80px;
+        position: relative;
+        border-radius: 50%;
+        box-sizing: content-box;
+        border: 4px solid #4CAF50;
+
+        &::before {
+            top: 3px;
+            left: -2px;
+            width: 30px;
+            transform-origin: 100% 50%;
+            border-radius: 100px 0 0 100px;
+        }
+
+        &::after {
+            top: 0;
+            left: 30px;
+            width: 60px;
+            transform-origin: 0 50%;
+            border-radius: 0 100px 100px 0;
+            animation: rotate-circle 4.25s ease-in;
+        }
+
+        &::before, &::after {
+            content: '';
+            height: 100px;
+            position: absolute;
+            background: #FFFFFF;
+            transform: rotate(-45deg);
+        }
+
+        .icon-line {
+            height: 5px;
+            background-color: #4CAF50;
+            display: block;
+            border-radius: 2px;
+            position: absolute;
+            z-index: 10;
+
+            &.line-tip {
+                top: 46px;
+                left: 14px;
+                width: 25px;
+                transform: rotate(45deg);
+                animation: icon-line-tip 0.75s;
+            }
+
+            &.line-long {
+                top: 38px;
+                right: 8px;
+                width: 47px;
+                transform: rotate(-45deg);
+                animation: icon-line-long 0.75s;
+            }
+        }
+
+        .icon-circle {
+            top: -4px;
+            left: -4px;
+            z-index: 10;
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+            position: absolute;
+            box-sizing: content-box;
+            border: 4px solid rgba(76, 175, 80, .5);
+        }
+
+        .icon-fix {
+            top: 8px;
+            width: 5px;
+            left: 26px;
+            z-index: 1;
+            height: 85px;
+            position: absolute;
+            transform: rotate(-45deg);
+            background-color: #FFFFFF;
+        }
+    }
+}
+
+@keyframes rotate-circle {
+    0% {
+        transform: rotate(-45deg);
+    }
+    5% {
+        transform: rotate(-45deg);
+    }
+    12% {
+        transform: rotate(-405deg);
+    }
+    100% {
+        transform: rotate(-405deg);
+    }
+}
+
+@keyframes icon-line-tip {
+    0% {
+        width: 0;
+        left: 1px;
+        top: 19px;
+    }
+    54% {
+        width: 0;
+        left: 1px;
+        top: 19px;
+    }
+    70% {
+        width: 50px;
+        left: -8px;
+        top: 37px;
+    }
+    84% {
+        width: 17px;
+        left: 21px;
+        top: 48px;
+    }
+    100% {
+        width: 25px;
+        left: 14px;
+        top: 45px;
+    }
+}
+
+@keyframes icon-line-long {
+    0% {
+        width: 0;
+        right: 46px;
+        top: 54px;
+    }
+    65% {
+        width: 0;
+        right: 46px;
+        top: 54px;
+    }
+    84% {
+        width: 55px;
+        right: 0px;
+        top: 35px;
+    }
+    100% {
+        width: 47px;
+        right: 8px;
+        top: 38px;
+    }
 }
 
 </style>
